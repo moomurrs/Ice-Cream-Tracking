fe76366800058cad5bafdb3e18680a68df45340e
MT <murun1life@gmail.com>
2025-04-06 10:37:36 -0400

    add basic router

---
+++ controllers/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$heading = 'Home';
+//var_dump($_SERVER['REQUEST_URI']);
+
+require "../views/index.view.php";
+
--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+require "utility/utility.php";
+
 $heading = 'Home';
 //var_dump($_SERVER['REQUEST_URI']);
-require "./views/index.view.php";
+
+$uri = parse_url($_SERVER['REQUEST_URI']);
+
+//dd($uri);
+
+
+if($uri['path'] == '/'){
+    require "./views/index.view.php";
+}
+
