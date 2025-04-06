03ed24dd2d5b486e4c2ae1b5651208e0311ae803
MT <murun1life@gmail.com>
2025-04-06 14:13:59 -0400

    make router its own file

--- index.php
+++ index.php
@@ -2,30 +2,4 @@
 
 require "utility/utility.php";
 
-$heading = 'Home';
-//var_dump($_SERVER['REQUEST_URI']);
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-//dd($uri);
-
-// array of routing mappings
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-];
-
-//var_dump($uri);
-//var_dump('<hr>');
-//var_dump($routes);
-//var_dump('<hr>');
-
-
-// neat little trick I saw, only go to page if it exists in array
-if(array_key_exists($uri, $routes)){
-    require $routes[$uri];
-} else{
-    http_response_code(404);
-    require 'views/404.php';
-    die(); // necessary?
-}
+require "router.php";
---
+++ router.php
@@ -0,0 +1,27 @@
+<?php
+//var_dump($_SERVER['REQUEST_URI']);
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+//dd($uri);
+
+// array of routing mappings
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+];
+
+//var_dump($uri);
+//var_dump('<hr>');
+//var_dump($routes);
+//var_dump('<hr>');
+
+
+// neat little trick I saw, only go to page if it exists in array
+if(array_key_exists($uri, $routes)){
+    require $routes[$uri];
+} else{
+    http_response_code(404);
+    require 'views/404.php';
+    die(); // necessary?
+}
