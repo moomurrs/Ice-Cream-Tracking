15c2863f95ba2e835695476a106a11008c33265e
MT <murun1life@gmail.com>
2025-04-06 21:42:32 -0400

    add more cards, add nice footer

--- views/index.view.php
+++ views/index.view.php
@@ -6,7 +6,7 @@
         <div class="card-header">Welcome!</div>
         <div class="card-body">
             <h4 class="card-title">Taster Beware, You're In For a Scare</h4>
-            <p class="card-text fs-3">Here at <span>Jojo's</span> <span>Bizzare</span> Ice Cream shop 🍦, we serve a range of strange and bizzare flavors to whet your curiosity! 😱
+            <p class="card-text fs-3">Here at <span class="fw-bold">Jojo's</span> <span class="fst-italic">Bizzare</span> Ice Cream shop 🍦, we serve a range of strange and bizzare flavors to whet your curiosity! 😱
                 Ever read Ripley's <span class="fst-italic">Believe or Not!&#8482;</span> and wasn't grossed out? Do you like the random nature of Jelly Beans&#8482;? Come and try the horrors with your taste-buds! 😎</p>
         </div>
     </div>
@@ -28,16 +28,32 @@
                 <div class="card-body">
                     <h4 class="card-title">Ready to Order?</h4>
                     <p class="card-text fs-3">
-                        Place your order here for 
+                        Place your order here! Try making a custom ice cream of your choice! Use our ordering system to choose your pickup time.
                     </p>
                 </div>
             </div>
         </div>
         <div class="col">
-            <div class="card">c</div>
+            <div class="card border-warning">
+                <div class="card-header">Gallery</div>
+                <div class="card-body">
+                    <h4 class="card-title">Not convinced? Check out our picture gallery of what's available!</h4>
+                    <p class="card-text fs-3">
+                        Here's a small selection to peek your
+                    </p>
+                </div>
+            </div>
         </div>
         <div class="col">
-            <div class="card">d</div>
+            <div class="card border-danger mb-4 mb-md-5">
+                <div class="card-header">Contact</div>
+                <div class="card-body">
+                    <h4 class="card-title">Catering, Questions, or Suggestions?</h4>
+                    <p class="card-text fs-3">
+                        We'd love to get in touch! DM for us catering events, ill-fated flavor suggestions, and other questions. See our other socials here.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 
--- views/partials/foot.php
+++ views/partials/foot.php
@@ -1,13 +1,13 @@
-<footer class="mt-auto">
+<footer class="mt-auto bg-primary px-3 py-2">
 
     <div class="row row-cols-2">
 
-        <div class="col">
-            <div class="card">a</div>
+        <div class="col text-center">
+            <div class="">JoJo's Bizzare Ice Cream made by MT</div>
         </div>
 
-        <div class="col">
-            <div class="card">Copyright 2025</div>
+        <div class="col text-end">
+            <div class="">Copyright 2025</div>
         </div>
     </div>
 </footer>
