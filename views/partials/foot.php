2a042fc5a5566ffdd3831982fed33134d4b44c4e
MT <murun1life@gmail.com>
2025-04-20 11:51:09 -0400

    add hours to footer

--- views/partials/foot.php
+++ views/partials/foot.php
@@ -1,13 +1,33 @@
 <footer class="mt-auto bg-primary px-3 py-2">
 
-    <div class="row row-cols-2">
+    <div class="row row-cols-2 pb-1">
 
-        <div class="col text-center">
-            <div class="">JoJo's Bizzare Ice Cream made by MT</div>
+        <div class="col text-end">
+            <div class="container text-center">
+                <div class="row row-cols-2">
+                    <div class="col text-end">Hours:</div>
+                    <div class="col text-start pb-1">Tue-Fri: 3pm - 9pm</div>
+                    <div class="col"></div>
+                    <div class="col text-start text-nowrap">Sat-Sun: 12pm - 10pm</div>
+                </div>
+            </div>
+
         </div>
 
         <div class="col text-end">
-            <div class="">Copyright 2025</div>
+            <p style="white-space: pre-line;">123 Street
+                Bloomington, IN 47401</p>
+        </div>
+    </div>
+
+    <div class="row row-cols-2">
+
+        <div class="col text-center">
+            <p class="">JoJo's Bizzare Ice Cream made by MT</p>
+        </div>
+
+        <div class="col text-end">
+            <p class="">Copyright 2025</p>
         </div>
     </div>
 </footer>
