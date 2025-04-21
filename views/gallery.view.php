bdd8e7ffa82b67f99eb6d6153792c98344ae7e28
MT <murun1life@gmail.com>
2025-04-21 01:06:31 -0400

    add gallery formatting

--- views/gallery.view.php
+++ views/gallery.view.php
@@ -2,6 +2,38 @@
 <?php require "partials/banner.php"; ?>
 
 <main class="container">
+
+    <div class="row">
+        <div class="col-md-6 col-xl-4">
+            <div class="card border-primary mb-4">
+                <div class="card-header">????</div>
+                <div class="card-body">
+                    <h4 class="card-title">????</h4>
+                    <p>????</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-4">
+            <div class="card border-primary mb-4">
+                <div class="card-header">????</div>
+                <div class="card-body">
+                    <h4 class="card-title">????</h4>
+                    <p>????</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-4">
+            <div class="card border-primary mb-4">
+                <div class="card-header">????</div>
+                <div class="card-body">
+                    <h4 class="card-title">????</h4>
+                    <p>????</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="card border-primary mb-4">
         <div class="card-header">????</div>
         <div class="card-body">
