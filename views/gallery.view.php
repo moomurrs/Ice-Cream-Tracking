5c037f8ccf8bcf7313e0a7a429a0265b9a5ba9f2
MT <murun1life@gmail.com>
2025-04-26 13:07:54 -0400

    update pictures

--- views/gallery.view.php
+++ views/gallery.view.php
@@ -6,41 +6,37 @@
     <div class="row">
         <div class="col-md-6 col-xl-4">
             <div class="card border-primary mb-4">
-                <div class="card-header">????</div>
                 <div class="card-body">
-                    <h4 class="card-title">????</h4>
-                    <p>????</p>
+                    <img src="./img/icecream1.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
         <div class="col-md-6 col-xl-4">
             <div class="card border-primary mb-4">
-                <div class="card-header">????</div>
                 <div class="card-body">
-                    <h4 class="card-title">????</h4>
-                    <p>????</p>
+                    <img src="./img/icecream2.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
+
         <div class="col-md-6 col-xl-4">
             <div class="card border-primary mb-4">
-                <div class="card-header">????</div>
                 <div class="card-body">
-                    <h4 class="card-title">????</h4>
-                    <p>????</p>
+                    <img src="./img/icecream3.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
-    </div>
 
-
-    <div class="card border-primary mb-4">
-        <div class="card-header">????</div>
-        <div class="card-body">
-            <h4 class="card-title">????</h4>
-            <p>????</p>
+        <div class="col-md-6 col-xl-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body">
+                    <img src="./img/icecream4.jpg" class="img-fluid">
+                </div>
+            </div>
         </div>
+
     </div>
+
 
 </main>
 
