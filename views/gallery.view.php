8375967cf84e6681da64e63752735b104ccb580f
MT <murun1life@gmail.com>
2025-04-26 13:21:20 -0400

    add responsive 1 col, 2col, then 3col

--- views/gallery.view.php
+++ views/gallery.view.php
@@ -1,40 +1,51 @@
 <?php require "partials/head.php"; ?>
 <?php require "partials/banner.php"; ?>
 
-<main class="container">
+<main class="container text-center">
 
     <div class="row">
-        <div class="col-md-6 col-xl-4">
+        <div class="col-sm-12 col-md-6 col-lg-4">
             <div class="card border-primary mb-4">
                 <div class="card-body">
                     <img src="./img/icecream1.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-xl-4">
+        <div class="col-sm-12 col-md-6 col-lg-4">
             <div class="card border-primary mb-4">
                 <div class="card-body">
                     <img src="./img/icecream2.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
-
-        <div class="col-md-6 col-xl-4">
+        <div class="col-sm-12 col-md-6 col-lg-4">
             <div class="card border-primary mb-4">
                 <div class="card-body">
                     <img src="./img/icecream3.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
-
-        <div class="col-md-6 col-xl-4">
+        <div class="col-sm-12 col-md-6 col-lg-4">
             <div class="card border-primary mb-4">
                 <div class="card-body">
                     <img src="./img/icecream4.jpg" class="img-fluid">
                 </div>
             </div>
         </div>
-
+        <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body">
+                    <img src="./img/icecream5.jpg" class="img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card border-primary mb-4">
+                <div class="card-body">
+                    <img src="./img/icecream6.jpg" class="img-fluid">
+                </div>
+            </div>
+        </div>
     </div>
 
 
