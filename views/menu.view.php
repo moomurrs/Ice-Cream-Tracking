86f69b8dfb560143edd55457e74177de0fa50748
MT <murun1life@gmail.com>
2025-04-27 09:05:24 -0400

    add menu template

--- views/menu.view.php
+++ views/menu.view.php
@@ -2,11 +2,27 @@
 <?php require "partials/banner.php"; ?>
 
 <main class="container">
-    <div class="card border-primary mb-4">
-        <div class="card-header">????</div>
-        <div class="card-body">
-            <h4 class="card-title">????</h4>
-            <p>????</p>
+    <div class="row row-cols-1 row-cols-xl-3 gx-4">
+        <div class="col">
+            <div class="card border-primary mb-4">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border-primary mb-4">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card border-primary mb-4">
+                <div class="card-body">
+                    
+                </div>
+            </div>
         </div>
     </div>
 
