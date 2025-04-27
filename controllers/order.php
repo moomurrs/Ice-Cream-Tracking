ec010ac3b496ee679e1870f91699a52987b6c90c
MT <murun1life@gmail.com>
2025-04-27 12:21:20 -0400

    submit to self

--- controllers/order.php
+++ controllers/order.php
@@ -2,9 +2,22 @@
 session_start();
 require "{$root}/models/pricing.model.php";
 
-
 $heading = 'Order';
 $title = 'Order';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    var_dump($_REQUEST);
+    die();
+
+    if (!empty($_POST['order-submit'])) {
+        var_dump($_POST['order-submit']);
+        die();
+        //dd($_POST['contact-submit']);
+    } else {
+        print_r("empty");
+        die();
+    }
+}
 
 require "{$root}/views/order.view.php";
