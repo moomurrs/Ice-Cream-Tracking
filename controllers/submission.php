9789ab82864202bae3c5c7356f15c0def58cf74d
MT <murun1life@gmail.com>
2025-04-30 09:46:18 -0400

    add working initial select

--- controllers/submission.php
+++ controllers/submission.php
@@ -14,9 +14,27 @@ $test_order = [
 $order = $test_order;
 $test_time = 1483228510;
 
-$time_epoch = time() + (60 * 15);;
 
-$date = new DateTime("@$time_epoch");
+$pdo = new PDO('sqlite:database/db.sqlite');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query = "SELECT * FROM Orders WHERE order_id = :order_id LIMIT 1";
+
+$statement = $pdo->prepare($query);
+
+$statement->execute([
+    ':order_id' => $order_id,
+]);
+
+$pdo = null; // close
+
+var_dump($statement->fetchAll(PDO::FETCH_ASSOC));
+
+die();
+
+$time_placed = $test_time;
+
+$date = new DateTime("@$time_placed");
 $date->setTimezone(new DateTimeZone("America/Indiana/Indianapolis"));
 $human_time = $date->format("g:i");
 
